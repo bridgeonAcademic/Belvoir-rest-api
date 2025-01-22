20f1bdf31e48d80124fc038dfcda1fc32674d08a
Mohammed-Nabeel-K <nabeelmohammed734@gmail.com>
2025-01-22 11:58:18 +0530

    design model added

---
+++ Belvoir.DAL/Models/Design.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Belvoir.DAL.Models
+{
+    public class Design
+    {
+        public string DesignId { get; set; }
+        public string DesignName { get; set; }
+        public string DesignDescription { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public bool Available { get; set; }
+        public List<string> ImageUrls { get; set; } 
+        public DateTime DesignCreatedAt { get; set; }
+    }
+}
