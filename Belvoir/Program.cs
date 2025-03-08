8cf36eb923a0444fd2c6678cf5da2612a4f6c936
Mohammed-Nabeel-K <nabeelmohammed734@gmail.com>
2025-03-08 16:27:33 +0530

    cookie

--- Belvoir/Program.cs
+++ Belvoir/Program.cs
@@ -178,7 +178,7 @@ if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseCors("AllowAllOrigins");
+app.UseCors("AllowFrontend");
 
 app.UseHttpsRedirection();
 
